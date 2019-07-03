a943c11d15d9ba0f94e4350ac2d2cd8474dc210f
f10r3nz4 <48447051+f10r3nz4@users.noreply.github.com>
2019-07-03 14:09:54 +0200

    Add files via upload

---
+++ ProjOggetti/src/main/java/com/progetto/ProjOggetti/Study.java
@@ -0,0 +1,47 @@
+package com.progetto.ProjOggetti;
+
+public class Study {
+	private String start;
+	private float length;
+	private float grant;
+	private int credits;
+	
+	public Study(String start, float length, float grant, int credits) {
+		this.start = start;
+		this.length = length;
+		this.grant = grant;
+		this.credits = credits;
+	}
+
+	public String getStart() {
+		return start;
+	}
+
+	public void setStart(String start) {
+		this.start = start;
+	}
+
+	public float getLength() {
+		return length;
+	}
+
+	public void setLength(float length) {
+		this.length = length;
+	}
+
+	public float getGrant() {
+		return grant;
+	}
+
+	public void setGrant(float grant) {
+		this.grant = grant;
+	}
+
+	public int getCredits() {
+		return credits;
+	}
+
+	public void setCredits(int credits) {
+		this.credits = credits;
+	}
+}
